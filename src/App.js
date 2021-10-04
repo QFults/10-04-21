85040b606bb4d0c2a11ba29c280fdd7efa983555
Quinton Fults <quinton.fults.work@gmail.com>
2021-10-04 10:59:24 -0700

    code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 const App = () => {
   return (
-    <>
-      <h1>Hello World!</h1>
-      <button>Click Me</button>
-    </>
+    <h1>New App</h1>
   )
 }
 
---
+++ src1/App.js
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <button>Click Me</button>
+    </>
+  )
+}
+
+export default App
